4a18d6bfb0883603be01209547d312e68dd869e7
oliver <oliver@locationlabs.com>
2012-05-10 22:33:40 -0700

    Way to import initial data

--- lunch_it/initdata.py
+++ lunch_it/initdata.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import os
 import sys
+import csv
+import urllib
+import xml.etree.ElementTree
 
 if __name__ == "__main__":
    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lunch_it.settings")
@@ -11,21 +14,29 @@ if __name__ == "__main__":
 
    from trains.models import Restaurant, Office
 
-   Restaurant(
-         name = "Ruby's", address = "6233 Hollis Street, Emeryville, CA",
-         latitude = "37.843351", longitude = "-122.290492",
-         walkable = True).save()
-   Restaurant(
-         name = "Liba Falafel", address = "6355 Hollis Street, Emeryville, CA",
-         latitude = "37.834073", longitude = "-122.287681",
-         url = "http://libasf.com/",
-         menu_url = "http://libasf.com/eat.html",
-         walkable = True).save()
-   Restaurant(
-         name = "Berkeley Bowl West", address = "920 Heinz Avenue, Berkeley, CA",
-         latitude = "37.853441", longitude = "-122.289762",
-         url = "http://www.berkeleybowl.com/",
-         walkable = False).save()
+   r = csv.reader(open('restaurants.csv'))
+   for row in r:
+      address = row[1]
+
+      params = urllib.urlencode({'address' : address, 'sensor' : 'true'})
+
+      geo_content = urllib.urlopen('http://maps.googleapis.com/maps/api/geocode/xml?%s' % params)
+      geo_content_text = geo_content.read()
+      print geo_content_text
+      geo_xml = xml.etree.ElementTree.XML(geo_content_text)
+      location_xml = geo_xml.find('result').find('geometry').find('location')
+      lat = location_xml.find('lat').text
+      lon = location_xml.find('lng').text
+
+      # not currently used
+      cuisines = [ c.strip() for c in row[5].split(',') ]
+
+      restaurant = Restaurant(
+            name = row[0], address = address, url = row[2],
+            phone = row[3], notes = row[4],
+            latitude = lat, longitude = lon)
+
+      restaurant.save()
 
    Office(
          name = "LLHQ",
