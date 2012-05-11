9b6287f71c567190d8b2bec909298b3732e9e598
Jacob Green <JacobGreen@max-osxlt.engr.wavemarket.com>
2012-05-10 18:21:37 -0700

    Added project Added trains app which will contain stuff for tracking which restaurants are availabe and which trains have formed so far now

---
+++ lunch_it/lunch_it/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'lunch_it.views.home', name='home'),
+    # url(r'^lunch_it/', include('lunch_it.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
