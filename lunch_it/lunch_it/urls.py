5bb85f61df81b053100df413187ffedcd4fb2088
Jacob Green <JacobGreen@max-osxlt.engr.wavemarket.com>
2012-05-10 19:20:11 -0700

    added bit about static stuff to  urls

--- lunch_it/lunch_it/urls.py
+++ lunch_it/lunch_it/urls.py
@@ -15,3 +15,10 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+# This lets you use static files folder on development environments
+if settings.DEBUG:
+    # static files (images, css, javascript, etc.)
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT}))
